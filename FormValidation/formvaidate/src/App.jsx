0c97439376d2c2257641293e60f436ca499dc38d
Ajay Mourya <mouryaajay7548@gmail.com>
2025-05-22 17:02:50 +0530

    updated

--- FormValidation/formvaidate/src/App.jsx
+++ FormValidation/formvaidate/src/App.jsx
@@ -1,34 +1,75 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+  const [emailError,setEmailError] = useState('')
+  const [passwordError,setPasswordError] = useState('')
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let valid = true;
+
+    setEmailError('')
+    setPasswordError('')
+  
+    if(!email){
+      setEmailError('Email is required')
+      valid=false
+    }else if(!emailRegex.test(email)){
+      setEmailError('Enter valid email')
+      valid=false
+    }
+
+    if(!password){
+      setPasswordError('Password is required')
+      valid = false
+    }else if(password.length < 6) {
+      setPasswordError('Password must be greater than 6 char')
+      valid = false
+    }
+
+    if(valid) {
+      alert('form submitted')
+      setEmail('')
+      setPassword('')
+    }
+
+  };
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div style={{display:'flex',flexDirection:'column',gap:'5px'
+        }}>
+          <label style={{fontSize:'20px'}}>Email: </label>
+          <input type="text"
+          value={email}
+          placeholder='Email..'
+          onChange={(e)=>setEmail(e.target.value)}
+          />
+        </div>  <br />
+        {emailError && <p style={{color:'red'}}>{emailError}</p> }
+
+        <div style={{display:'flex',flexDirection:'column',gap:'5px'}}>
+          <label style={{fontSize:'20px'}}>Password: </label>
+          <input type="text"
+          value={password}
+          placeholder='password...'
+          onChange={(e)=>setPassword(e.target.value)}
+          />
+        </div>
+        <br />
+        {passwordError && <p style={{color:'red'}}>{passwordError}</p> }
+        <button type='submit'>
+          Submit
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </form>
+    </div>
   )
 }
 
